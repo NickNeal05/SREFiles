969c85d114ab4aaf4013969d246c9c0c0491e107
jpinachoSyntel <joseph_pinacho@syntelinc.com>
2017-02-09 14:58:02 -0500

    JS File
    Added Table.js

---
+++ src/main/webapp/WEB-INF/Table.js
@@ -0,0 +1,24 @@
+var app = angular.module("myApp",[]);
+
+app.controller("myController",function($scope)
+{
+	$scope.group = [{name:"Joseph Pinacho",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
+	                {name:"Kiara Delepine",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
+	                {name:"Jason Morris",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
+	                {name:"Nicholas Neal",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
+	                {name:"Jonathan Sullivan",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
+	                {name:"Xavier Smith",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1}];
+	
+	sortByKey($scope.group,'name');
+	
+	function sortByKey(arr,key)
+	{
+		return arr.sort(function(a,b)
+		{
+			var x = a[key];
+			var y = b[key];
+			
+			return ((x < y) ? -1: ((x > y) ? 1:0));
+		});
+	}
+});
