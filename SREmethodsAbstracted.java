975950de0b611299f5e7f941cf16cf34bc99bf55
KiaraD615 <kiara_delepine@syntelinc.com>
2017-02-09 15:22:16 -0500

    Update SREmethodsAbstracted.java

--- SREmethodsAbstracted.java
+++ SREmethodsAbstracted.java
@@ -1,9 +1,11 @@
 import java.io.IOException;
 
+import org.openqa.selenium.StaleElementReferenceException;
+
 
 public class SREmethodsAbstracted {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, StaleElementReferenceException {
 		SREwebdriverScript SRE = new SREwebdriverScript();
 		SRE.open();
 		SRE.getTitle();
