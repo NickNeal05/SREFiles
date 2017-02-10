cab53999feb34989a42270144b6acafd49ba090c
macosx <macosx@macosxs-MacBook-Air.local>
2017-02-10 10:22:01 -0500

    Added plumbing to jsp

--- src/main/java/sreTraining/Servlet.java
+++ src/main/java/sreTraining/Servlet.java
@@ -1,8 +1,18 @@
 package sreTraining;
 
-public class Servlet {
-	public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class Servlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+		getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
+	}
 }
