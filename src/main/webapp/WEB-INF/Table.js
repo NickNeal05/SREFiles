edbf9cd4217774b2d21e9727b995f521d3be5b7f
KiaraD615 <kiara_delepine@syntelinc.com>
2017-02-09 20:57:07 -0500

    Edited table to contain null & outOfRange values
    For testing purposes

--- src/main/webapp/WEB-INF/Table.js
+++ src/main/webapp/WEB-INF/Table.js
@@ -2,12 +2,12 @@ var app = angular.module("myApp",[]);
 
 app.controller("myController",function($scope)
 {
-	$scope.group = [{name:"Joseph Pinacho",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
-	                {name:"Kiara Delepine",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
-	                {name:"Jason Morris",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
+	$scope.group = [{name:"Joseph Pinacho",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:0,jenkins:1,pcf:1},
+	                {name:"Kiara Delepine",java:9,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
+	                {name:"Jason Morris",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:7,jenkins:1,pcf:1},
 	                {name:"Nicholas Neal",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
-	                {name:"Jonathan Sullivan",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1},
-	                {name:"Xavier Smith",java:4,webApps:1,ajs:4,bstrp:1,junit:3,mockito:1,jenkins:1,pcf:1}];
+	                {name:"Jonathan Sullivan",java:4,webApps:1,ajs:4,bstrp:1,junit:null,mockito:1,jenkins:1,pcf:1},
+	                {name:"Xavier Smith",java:4,webApps:1,ajs:4,bstrp:-2,junit:3,mockito:1,jenkins:1,pcf:1}];
 	
 	sortByKey($scope.group,'name');
 	
